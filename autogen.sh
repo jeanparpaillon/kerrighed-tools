e6a2b6aa94d4af6a69b2ce3f7ecc5e102dc1460a
Jean Parpaillon <jean.parpaillon@kerlabs.com>
2009-07-02 16:46:15 +0200

    Backport build system from 2.3.0

--- autogen.sh
+++ autogen.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 # Copyright 2006-2007 INRIA, All rights reserved
+# Copyright 2009 Kerlabs, All rights reserved
 #
 # Authors:
-#    Jean Parpaillon <jean.parpaillon@kerlabs.com>
+#   Jean Parpaillon <jean.parpaillon@kerlabs.com>
 #
 set -e
 
 $(dirname $0)/clean.sh
 
 #
-# check tools version number -- we require >= 1.9
+# check tools version number -- we require >= 1.10
 #
 find_tools() {
     tool=$1
 
-    if `which $tool-1.9 > /dev/null 2>&1`; then
-	TOOL=$tool-1.9
+    if `which $tool-1.10 > /dev/null 2>&1`; then
+        TOOL=$tool-1.10
     else
-	major=`$tool --version | grep $tool | awk {'print \$4'} | awk -F '.' {'print \$1'}`
-	minor=`$tool --version | grep $tool | awk {'print \$4'} | awk -F '.' {'print \$2'}`
-	if test "$major" -gt 1; then
-	    TOOL=$tool
-	elif test "$major" -eq 1 -a "$minor" -ge 9; then
-	    TOOL=$tool
-	else
-	    echo "Required: $tool version >= 1.9"
-	    exit 1;
-	fi
+        major=`$tool --version | grep $tool | awk {'print \$4'} | awk -F '.' {'print \$1'}`
+        minor=`$tool --version | grep $tool | awk {'print \$4'} | awk -F '.' {'print \$2'}`
+        if test "$major" -gt 1; then
+            TOOL=$tool
+        elif test "$major" -eq 1 -a "$minor" -ge 10; then
+            TOOL=$tool
+        else
+            echo "Required: $tool version >= 1.10"
+            exit 1;
+        fi
     fi
 
     echo "$TOOL"
@@ -37,13 +38,12 @@ AUTOMAKE=$(find_tools automake)
 
 if test -f $(dirname $0)/configure.ac; then
     (
-	cd $(dirname $0)
-	echo "Regenerating autoconf/libtoolize files"
-	$ACLOCAL -I m4
-	libtoolize -c
-	autoheader
-	#$AUTOMAKE --add-missing --foreign
-	$AUTOMAKE --add-missing
-	autoconf
-	)
+        cd $(dirname $0)
+        echo "Regenerating autoconf/libtoolize files"
+        $ACLOCAL -I m4
+        libtoolize -c
+        autoheader
+        $AUTOMAKE --add-missing
+        autoconf
+        )
 fi
--- clean.sh
+++ clean.sh
@@ -3,12 +3,13 @@
 # Copyright 2006-2007 INRIA, All rights reserved
 # Copyright 2009 Kerlabs, All rights reserved
 #
-# Author:
+# Authors:
 #   Jean Parpaillon <jean.parpaillon@kerlabs.com>
 #
 set -e
 
-AUTOGEN_FILES="aclocal.m4 autom4te.cache configure config.guess config.log config.sub config.status depcomp install-sh compile libtool ltmain.sh missing mkinstalldirs config.h config.h.in m4/*"
+AUTOGEN_FILES="aclocal.m4 autom4te.cache configure config.guess config.log config.sub config.status depcomp install-sh compile libtool ltmain.sh missing mki\
+nstalldirs config.h config.h.in m4/*"
 
 echo "Clean autogen generated files"
 for file in $AUTOGEN_FILES; do
