f0b542ee2054f18f656326ba274b08b197012cf7
Jean Parpaillon <jean.parpaillon@kerlabs.com>
2009-12-17 18:39:24 +0100

    [libhotplug] Provides complete API for data structures.
    	Add support for TIPC nodes status
    	Upgrade lib major version to 2

--- libs/include/hotplug.h
+++ libs/include/hotplug.h
@@ -1,7 +1,12 @@
 #ifndef __LIB_HOTPLUG__
 #define __LIB_HOTPLUG__
 
-#include <stdio.h>
+enum krg_status {
+	HOTPLUG_NODE_INVALID,
+	HOTPLUG_NODE_POSSIBLE,
+	HOTPLUG_NODE_PRESENT,
+	HOTPLUG_NODE_ONLINE,
+};
 
 struct krg_nodes {
 	char* nodes;
@@ -16,23 +21,184 @@ struct krg_node_set {
 	char* v;
 };
 
-int krg_get_max_nodes(void);
-int krg_get_max_clusters(void);
+extern int kerrighed_max_nodes;
+extern int kerrighed_max_clusters;
 
-static inline void krg_clear_node_set(struct krg_node_set *item){
-	int bcl;
+/*
+ * krg_hotplug_init
+ *
+ * Initialize some hotplug internals
+ *
+ * Return 0 on success, -1 on failure
+ */
+int krg_hotplug_init(void);
 
-	for(bcl=0;bcl<krg_get_max_nodes();bcl++)
-		item->v[bcl] = 0;
+/*
+ * krg_status_str
+ *
+ * Return the name of the status
+ */
+const char* krg_status_str(int s);
 
-}
+/*
+ * krg_node_set_create
+ *
+ * Creates and initializes a krg_node_set.
+ *
+ * Returns NULL on memory allocation error
+ */
+struct krg_node_set* krg_node_set_create(void);
 
-static inline void krg_node_set_add(struct krg_node_set *item, int n){
-	if(n>=0 && n<krg_get_max_nodes())
-		item->v[n] = 1;
-	else
-		printf("ERROR: krg_node_set_add: out of range (%d)\n", n);
-}
+/*
+ * krg_node_set_destroy
+ *
+ * Destroy a krg_node_set
+ */
+void krg_node_set_destroy(struct krg_node_set* node_set);
+
+/*
+ * krg_node_set_clear
+ *
+ * Reset node_set
+ */
+void krg_node_set_clear(struct krg_node_set* node_set);
+
+/*
+ * krg_node_set_add
+ *
+ * Add node with id n to node_set.
+ * n must be >= 0 and < kerrighed_max_nodes
+ *
+ * Returns 0 on success, -1 on failure (n out of range)
+ */
+int krg_node_set_add(struct krg_node_set* node_set, int n);
+
+/*
+ * krg_node_set_remove
+ *
+ * Remove node with id n from node_set.
+ * n must be >= 0 and < kerrighed_max_nodes
+ *
+ * Returns 0 on success, -1 on failure (n out of range)
+ */
+int krg_node_set_remove(struct krg_node_set* node_set, int n);
+
+/*
+ * krg_node_set_contains
+ *
+ * Returns 1 if node is present in the set, 0 if not
+ */
+int krg_node_set_contains(struct krg_node_set* node_set, int n);
+
+/*
+ * krg_node_set_weight
+ *
+ * Returns number of nodes in set
+ */
+int krg_node_set_weight(struct krg_node_set* node_set);
+
+/*
+ * krg_node_set_next
+ *
+ * Give -1 as n to initialize iterator
+ *
+ * Returns next node in the set, -1 if no next
+ */
+int krg_node_set_next(struct krg_node_set* node_set, int n);
+
+/*
+ * krg_nodes_create
+ *
+ * Creates and initializes a krg_nodes struct
+ */
+struct krg_nodes* krg_nodes_create(void);
+
+/*
+ * krg_nodes_destroy
+ *
+ * Destroy a krg_nodes
+ */
+void krg_nodes_destroy(struct krg_nodes *nodes);
+
+/*
+ * krg_nodes_num
+ *
+ * Return number of nodes in the given status
+ */
+int krg_nodes_num(struct krg_nodes* nodes, enum krg_status s);
+int krg_nodes_num_online(struct krg_nodes* nodes);
+int krg_nodes_num_possible(struct krg_nodes* nodes);
+int krg_nodes_num_present(struct krg_nodes* nodes);
+
+/*
+ * krg_nodes_is
+ * node must be >= 0 and < kerrighed_max_nodes
+ *
+ * Return 1 if node is in the given status, 0 otherwise, -1 in case of failure (n out of range)
+ */
+int krg_nodes_is(struct krg_nodes* nodes, int node, enum krg_status s);
+int krg_nodes_is_online(struct krg_nodes* nodes, int node);
+int krg_nodes_is_possible(struct krg_nodes* nodes, int node);
+int krg_nodes_is_present(struct krg_nodes* nodes, int node);
+
+/*
+ * krg_nodes_next
+ * node must be < kerrighed_max_nodes
+ * give -1 as node to initialize iterator
+ *
+ * Return node id in the given state following given node, -1 if no next
+ */
+int krg_nodes_next(struct krg_nodes* nodes, int node, enum krg_status s);
+int krg_nodes_next_online(struct krg_nodes* nodes, int node);
+int krg_nodes_next_possible(struct krg_nodes* nodes, int node);
+int krg_nodes_next_present(struct krg_nodes* nodes, int node);
+
+/*
+ * krg_nodes_get
+ *
+ * Return set of nodes in given state, NULL in case of failure (out of mem)
+ */
+struct krg_node_set* krg_nodes_get(struct krg_nodes* nodes, enum krg_status s);
+struct krg_node_set* krg_nodes_get_online(struct krg_nodes* nodes);
+struct krg_node_set* krg_nodes_get_possible(struct krg_nodes* nodes);
+struct krg_node_set* krg_nodes_get_present(struct krg_nodes* nodes);
+
+/*
+ * krg_nodes_getnode
+ *
+ * Return status of given node
+ */
+enum krg_status krg_nodes_getnode(struct krg_nodes* nodes, int node);
+
+/*
+ * krg_nodes_nextnode
+ * Give -1 as node to initialize iterator
+ *
+ * Return node id following given node which is not invalid, -1 if no next
+ */
+int krg_nodes_nextnode(struct krg_nodes* nodes, int node);
+
+/*
+ * krg_clusters_create
+ *
+ * Creates and initializes a krg_clusters struct
+ * Returns NULL on failure (out of mem)
+ */
+struct krg_clusters* krg_clusters_create(void);
+
+/*
+ * krg_clusters_destroy
+ *
+ * Destroy a krg_clusters
+ */
+void krg_clusters_destroy(struct krg_clusters* clusters);
+
+/*
+ * krg_clusters_is_up
+ *
+ * Return 1 if cluster is up, 0 otherwise, -1 in case of failure (invalid cluster id)
+ */
+int krg_clusters_is_up(struct krg_clusters* nodes, int cluster);
 
 int krg_nodes_add(struct krg_node_set *node_set);
 int krg_nodes_remove(struct krg_node_set *node_set);
@@ -42,6 +208,7 @@ struct krg_nodes* krg_nodes_status(void);
 struct krg_clusters* krg_cluster_status(void);
 int krg_set_cluster_creator(int enable);
 int krg_cluster_start(struct krg_node_set *krg_node_set);
+int krg_cluster_start_all(void);
 int krg_cluster_wait_for_start(void);
 int krg_node_ready(int setup_ok);
 int krg_cluster_shutdown(int subclusterid);
--- libs/libkerrighed/libhotplug.c
+++ libs/libkerrighed/libhotplug.c
@@ -1,60 +1,377 @@
-/** Cluster configuration related interface functions.
- *  @file libhotplug.c
+/* Cluster configuration related interface functions.
+ * @file libhotplug.c
  *
- *  Copyright (C) 2006-2007, Pascal Gallard, Kerlabs.
+ * Copyright (C) 2006-2007, Kerlabs
+ *
+ * Authors:
+ *    Pascal Gallard, <pascal.gallard@kerlabs.com>
+ *    Jean Parpaillon, <jean.parpaillon@kerlabs.com>
  */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <sys/syscall.h>
 #include <errno.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <string.h>
 
 #include <types.h>
 #include <krgnodemask.h>
 #include <kerrighed_tools.h>
 #include <hotplug.h>
 
-static int kerrighed_max_nodes = -1;
-static int kerrighed_max_clusters = -1;
+int kerrighed_max_nodes = -1;
+int kerrighed_max_clusters = -1;
 
-int krg_get_max_nodes(void){
+const char* krg_status_str(int s)
+{
+	static char *str[4] = { "invalid", "possible", "present", "online" };
+
+	if (s >= 0 && s <= HOTPLUG_NODE_ONLINE)
+		s = HOTPLUG_NODE_INVALID;
+
+	return str[s];
+}
+
+struct krg_nodes* krg_nodes_create(void)
+{
+	struct krg_nodes* item;
+
+	item = malloc(sizeof(struct krg_nodes));
+	if (!item) {
+		return NULL;
+	}
+
+	item->nodes = malloc(kerrighed_max_nodes);
+	if (!item->nodes) {
+		free(item);
+		return NULL;
+	}
+
+	return item;
+}
+
+void krg_nodes_destroy(struct krg_nodes* item)
+{
+	if (item) {
+		free(item->nodes);
+		free(item);
+	}
+}
+
+int krg_nodes_num(struct krg_nodes* nodes, enum krg_status s)
+{
+	int ret = 0;
+	int bcl;
+
+	for (bcl = 0; bcl < kerrighed_max_nodes; bcl++) {
+		if (nodes->nodes[bcl] == s)
+			ret++;
+	}
+
+	return ret;
+}
+
+int krg_nodes_num_possible(struct krg_nodes* nodes)
+{
+	return krg_nodes_num(nodes, HOTPLUG_NODE_POSSIBLE);
+}
+
+int krg_nodes_num_present(struct krg_nodes* nodes)
+{
+	return krg_nodes_num(nodes, HOTPLUG_NODE_PRESENT);
+}
+
+int krg_nodes_num_online(struct krg_nodes* nodes)
+{
+	return krg_nodes_num(nodes, HOTPLUG_NODE_ONLINE);
+}
+
+int krg_nodes_is(struct krg_nodes* nodes, int n, enum krg_status s)
+{
+	int ret = 0;
+
+	if (n >= 0 && n < kerrighed_max_nodes) {
+		if (nodes->nodes[n] == s)
+			ret = 1;
+	} else {
+		ret = -1;
+	}
+	
+	return ret;
+}
+
+int krg_nodes_is_possible(struct krg_nodes* nodes, int n)
+{
+	return krg_nodes_is(nodes, n, HOTPLUG_NODE_POSSIBLE);
+}
+
+int krg_nodes_is_present(struct krg_nodes* nodes, int n)
+{
+	return krg_nodes_is(nodes, n, HOTPLUG_NODE_PRESENT);
+}
+
+int krg_nodes_is_online(struct krg_nodes* nodes, int n)
+{
+	return krg_nodes_is(nodes, n, HOTPLUG_NODE_ONLINE);
+}
+
+int krg_nodes_next(struct krg_nodes* nodes, int node, enum krg_status s)
+{
+	int bcl;
+
+	bcl = node + 1;
+	while (bcl < kerrighed_max_nodes) {
+		if (nodes->nodes[bcl] == s)
+			return bcl;
+		bcl++;
+	}
+	return -1;
+}
+
+int krg_nodes_next_online(struct krg_nodes* nodes, int node)
+{
+	return krg_nodes_next(nodes, node, HOTPLUG_NODE_ONLINE);
+}
+
+int krg_nodes_next_possible(struct krg_nodes* nodes, int node)
+{
+	return krg_nodes_next(nodes, node, HOTPLUG_NODE_POSSIBLE);
+}
+
+int krg_nodes_next_present(struct krg_nodes* nodes, int node)
+{
+	return krg_nodes_next(nodes, node, HOTPLUG_NODE_PRESENT);
+}
+
+struct krg_node_set* krg_nodes_get(struct krg_nodes* nodes, enum krg_status s)
+{
+	struct krg_node_set* r = NULL;
+	int bcl;
+
+	r = krg_node_set_create();
+	if (r) {
+		for (bcl = 0; bcl < kerrighed_max_nodes; bcl++) {
+			if (nodes->nodes[bcl] == s)
+				krg_node_set_add(r, bcl);
+		}
+	}
+
+	return r;
+}
+
+struct krg_node_set* krg_nodes_get_online(struct krg_nodes* nodes)
+{
+	return krg_nodes_get(nodes, HOTPLUG_NODE_ONLINE);
+}
+
+struct krg_node_set* krg_nodes_get_possible(struct krg_nodes* nodes)
+{
+	return krg_nodes_get(nodes, HOTPLUG_NODE_POSSIBLE);
+}
+
+struct krg_node_set* krg_nodes_get_present(struct krg_nodes* nodes)
+{
+	return krg_nodes_get(nodes, HOTPLUG_NODE_PRESENT);
+}
+
+enum krg_status krg_nodes_getnode(struct krg_nodes* nodes, int n)
+{
+	if (n >= 0 && n < kerrighed_max_nodes) {
+		return nodes->nodes[n];
+	} else
+		return HOTPLUG_NODE_INVALID;
+}
+
+int krg_nodes_nextnode(struct krg_nodes* nodes, int node)
+{
+	int bcl;
+
+	bcl = node + 1;
+	while (bcl < kerrighed_max_nodes) {
+		if (nodes->nodes[bcl] > HOTPLUG_NODE_INVALID)
+			return bcl;
+		bcl++;
+	}
+	return -1;
+}
+
+struct krg_clusters* krg_clusters_create(void)
+{
+	struct krg_clusters* item;
+
+	item = malloc(sizeof(struct krg_clusters));
+	if (!item) {
+		return NULL;
+	}
+
+	item->clusters = malloc(kerrighed_max_clusters);
+	if (!item->clusters) {
+		free(item);
+		return NULL;
+	}
+
+	return item;
+}
+
+void krg_clusters_destroy(struct krg_clusters* item)
+{
+	if (item) {
+		free(item->clusters);
+		free(item);
+	}
+}
+
+int krg_clusters_is_up(struct krg_clusters* item, int n)
+{
+	if (n >= 0 && n < kerrighed_max_clusters) {
+		if (item->clusters[n])
+			return 1;
+		else
+			return 0;
+	} else
+		return -1;
+}
+
+struct krg_node_set* krg_node_set_create(void)
+{
+	struct krg_node_set* item;
+
+	item = malloc(sizeof(struct krg_node_set));
+	if (!item) {
+		return NULL;
+	}
+
+	item->v = malloc(kerrighed_max_nodes);
+	if (!item->v) {
+		free(item);
+		return NULL;
+	}
+
+	krg_node_set_clear(item);
+
+	return item;
+}
+
+void krg_node_set_destroy(struct krg_node_set* item)
+{
+	if (item) {
+		free(item->v);
+		free(item);
+	}
+}
+
+void krg_node_set_clear(struct krg_node_set* item)
+{
+	int bcl;
+
+	item->subclusterid = 0;
+	for (bcl = 0; bcl < kerrighed_max_nodes; bcl++)
+		item->v[bcl] = 0;
+}
+
+int krg_node_set_add(struct krg_node_set* item, int n)
+{
+	if (n >= 0 && n < kerrighed_max_nodes) {
+		item->v[n] = 1;
+		return 0;
+	} else
+		return -1;
+}
+
+int krg_node_set_remove(struct krg_node_set* item, int n)
+{
+	if (n >= 0 && n < kerrighed_max_nodes) {
+		item->v[n] = 0;
+		return 0;
+	} else
+		return -1;
+}
+
+int krg_node_set_contains(struct krg_node_set* node_set, int n)
+{
+	if (n >= 0 && n < kerrighed_max_nodes)
+		return node_set->v[n];
+	else
+		return 0;
+}
+
+int krg_node_set_weight(struct krg_node_set* node_set)
+{
+	int r, bcl;
+
+	r = 0;
+	for (bcl = 0; bcl < kerrighed_max_nodes; bcl++)
+		if (node_set->v[bcl])
+			r++;
+
+	return r;
+}
+
+int krg_node_set_next(struct krg_node_set* node_set, int node)
+{
+	int bcl;
+
+	bcl = node + 1;
+	while (bcl < kerrighed_max_nodes) {
+		if (node_set->v[bcl])
+			return bcl;
+		bcl++;
+	}
+	return -1;
+}
+
+int krg_get_max_nodes(void)
+{
 	int r;
 
 	if(kerrighed_max_nodes == -1){
 		r = call_kerrighed_services(KSYS_NB_MAX_NODES, &kerrighed_max_nodes);
-		if(r) return -1;
-	};
+		if(r) {
+			errno = EPERM;
+			return -1;
+		}
+	}
 
 	return kerrighed_max_nodes;
-};
+}
 
-int krg_get_max_clusters(void){
+int krg_get_max_clusters(void)
+{
 	int r;
 
 	if(kerrighed_max_clusters == -1){
 		r = call_kerrighed_services(KSYS_NB_MAX_CLUSTERS, &kerrighed_max_clusters);
-		if(r) return -1;
-	};
+		if(r) {
+			errno = EPERM;
+			return -1;
+		}
+	}
 
 	return kerrighed_max_clusters;
-};
+}
 
-int krg_nodes_add(struct krg_node_set *krg_node_set){
+int krg_hotplug_init(void)
+{
+	kerrighed_max_nodes = krg_get_max_nodes();
+	if (kerrighed_max_nodes == -1)
+		return -1;
+	kerrighed_max_clusters = krg_get_max_clusters();
+	if (kerrighed_max_clusters == -1)
+		return -1;
+
+	return 0;
+}
+
+int krg_nodes_add(struct krg_node_set *krg_node_set)
+{
 	struct hotplug_node_set node_set;
 	int i, r;
 
 	krgnodes_clear(node_set.v);
 	node_set.subclusterid = krg_node_set->subclusterid;
 	
-	for(i=0;i<krg_get_max_nodes();i++){
+	for (i = 0; i < kerrighed_max_nodes; i++) {
 		if(krg_node_set->v[i]){
 			krgnode_set(i, node_set.v);
-		};
-	};
+		}
+	}
 
 	r = call_kerrighed_services(KSYS_HOTPLUG_ADD, &node_set);
 	if (r) return -1;
@@ -62,25 +379,25 @@ int krg_nodes_add(struct krg_node_set *krg_node_set){
 	return 0;
 };
 
-int krg_nodes_remove(struct krg_node_set *krg_node_set){
+int krg_nodes_remove(struct krg_node_set *krg_node_set)
+{
 	struct hotplug_node_set node_set;
 	int i, r;
 
 	krgnodes_clear(node_set.v);
 	node_set.subclusterid = krg_node_set->subclusterid;
 	
-	for(i=0;i<krg_get_max_nodes();i++){
+	for(i = 0; i < kerrighed_max_nodes; i++) {
 		if(krg_node_set->v[i]){
-			printf("krg_nodes_remove: %d\n", i);
 			krgnode_set(i, node_set.v);
-		};
-	};
+		}
+	}
 
 	r = call_kerrighed_services(KSYS_HOTPLUG_REMOVE, &node_set);
 	if (r) return -1;
 
 	return 0;
-};
+}
 
 int krg_nodes_fail(struct krg_node_set *krg_node_set){
 	struct hotplug_node_set node_set;
@@ -88,83 +405,80 @@ int krg_nodes_fail(struct krg_node_set *krg_node_set){
 
 	krgnodes_clear(node_set.v);
 	
-	for(i=0;i<krg_get_max_nodes();i++){
-		if(krg_node_set->v[i]){
+	for (i = 0; i < kerrighed_max_nodes; i++) {
+		if (krg_node_set->v[i]){
 			krgnode_set(i, node_set.v);
-		};
-	};
+		}
+	}
 
 	r = call_kerrighed_services(KSYS_HOTPLUG_FAIL, &node_set);
 	if (r) return -1;
 
 	return 0;
-};
+}
 
-int krg_nodes_poweroff(struct krg_node_set *krg_node_set){
+int krg_nodes_poweroff(struct krg_node_set *krg_node_set)
+{
 	struct hotplug_node_set node_set;
 	int i, r;
 
 	krgnodes_clear(node_set.v);
 	
-	for(i=0;i<krg_get_max_nodes();i++){
-		if(krg_node_set->v[i]){
+	for (i = 0; i < kerrighed_max_nodes; i++) {
+		if (krg_node_set->v[i]) {
 			krgnode_set(i, node_set.v);
-		};
-	};
+		}
+	}
 
 	r = call_kerrighed_services(KSYS_HOTPLUG_POWEROFF, &node_set);
 	if (r) return -1;
 
 	return 0;
-};
+}
 
-struct krg_nodes*  krg_nodes_status(void){
+struct krg_nodes* krg_nodes_status(void)
+{
 	struct krg_nodes *krg_nodes;
 	struct hotplug_nodes hotplug_nodes;
 	int r;
 
-	krg_nodes = malloc(sizeof(struct krg_nodes));
-	if(!krg_nodes)
-		return NULL;
-	krg_nodes->nodes = malloc(sizeof(char)*krg_get_max_nodes());
-	if(!krg_nodes->nodes)
+	krg_nodes = krg_nodes_create();
+	if (!krg_nodes)
 		return NULL;
 	
 	hotplug_nodes.nodes = krg_nodes->nodes;
 	
 	r = call_kerrighed_services(KSYS_HOTPLUG_NODES, &hotplug_nodes);
 	
-	if(r){
-		free(krg_nodes->nodes);
-		free(krg_nodes);
+	if (r) {
+		krg_nodes_destroy(krg_nodes);
 		return  NULL;
-	};
+	}
 
 	return krg_nodes;
-};
+}
 
-struct krg_clusters* krg_cluster_status(void){
+struct krg_clusters* krg_cluster_status(void)
+{
 	struct krg_clusters *krg_clusters;
 	struct hotplug_clusters hotplug_clusters;
 	int r;
 
-	if(! (krg_clusters = malloc(sizeof(*krg_clusters))) )
+	krg_clusters = krg_clusters_create();
+	if (!krg_clusters)
 		return NULL;
-	if(! (krg_clusters->clusters = malloc(sizeof(char)*krg_get_max_clusters())))
-	      return NULL;
 	
 	r = call_kerrighed_services(KSYS_HOTPLUG_STATUS, &hotplug_clusters);
 
-	if(r){
-		free(krg_clusters->clusters);
-		free(krg_clusters);
+	if (r) {
+		krg_clusters_destroy(krg_clusters);
 		return NULL;
-	};
+	}
 
-	memcpy(krg_clusters->clusters, hotplug_clusters.clusters, krg_get_max_clusters());
+	memcpy(krg_clusters->clusters, hotplug_clusters.clusters, kerrighed_max_clusters);
 	
 	return krg_clusters;
-};
+}
 
 int krg_set_cluster_creator(int enable)
 {
@@ -174,24 +488,49 @@ int krg_set_cluster_creator(int enable)
 
 int krg_cluster_start(struct krg_node_set *krg_node_set){
 	struct hotplug_node_set node_set;
-	int i, r;
+	struct krg_clusters* clusters;
+	int i;
+
+	clusters = krg_cluster_status();
+	if (krg_clusters_is_up(clusters, 0)) {
+		errno = EALREADY;
+		return -1;
+	}
 
 	node_set.subclusterid = krg_node_set->subclusterid;
 
 	krgnodes_clear(node_set.v);
 	
-	for(i=0;i<krg_get_max_nodes();i++){
-		if(krg_node_set->v[i]){
+	for (i = 0; i < kerrighed_max_nodes; i++) {
+		if (krg_node_set->v[i]) {
 			krgnode_set(i, node_set.v);
-		};
-	};
+		}
+	}
 	
-	r = call_kerrighed_services(KSYS_HOTPLUG_START, &node_set);
-	if(r)
-		return -1;
-	
-	return 0;
-};
+	return call_kerrighed_services(KSYS_HOTPLUG_START, &node_set);
+}
+
+int krg_cluster_start_all(void)
+{
+	struct krg_nodes* status;
+	struct krg_node_set* nodes;
+	int r = -1;
+
+	status = krg_nodes_status();
+	if(! status)
+		return r;
+
+	nodes = krg_nodes_get_present(status);
+	if (! nodes)
+		krg_nodes_destroy(status);
+		return r;
+	r = krg_cluster_start(nodes);
+
+	krg_nodes_destroy(status);
+	krg_node_set_destroy(nodes);
+
+	return r;
+}
 
 int krg_cluster_wait_for_start(void)
 {
@@ -214,12 +553,7 @@ int krg_cluster_shutdown(int subclusterid){
 	return 0;
 };
 
-int krg_cluster_reboot(int subclusterid){
-	int r;
-
-	r = call_kerrighed_services(KSYS_HOTPLUG_RESTART, &subclusterid);
-	if(r)
-		return -1;
-	
-	return 0;
-};
+int krg_cluster_reboot(int subclusterid)
+{
+	return call_kerrighed_services(KSYS_HOTPLUG_RESTART, &subclusterid);
+}
