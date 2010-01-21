187bad1bd5034ee6fe896183608cf4c5830b7adb
Matthieu Fertré <matthieu.fertre@kerlabs.com>
2010-01-21 15:54:12 +0100

    bi-cr-disable: play with cr_exclude_on and cr_exclude_off

--- tests/apps/bi-cr-disable.c
+++ tests/apps/bi-cr-disable.c
@@ -1,13 +1,19 @@
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <kerrighed/kerrighed.h>
+#include <kerrighed/libkrgcheckpoint.h>
 #include "libbi.h"
 
 int numloops = -1 ;
 int quiet = 0;
 int close_stdbuffers = 0;
+
+const int MAX_BUFFER_SIZE = 4000000;
 
 void parse_args(int argc, char *argv[])
 {
@@ -46,27 +52,91 @@ void loop(int quiet, int numloops)
 {
 	int i = 0, j = 0;
 
-	for (j = 0; j < numloops/10; j++) {
+	for (j = 0; j < numloops; j++) {
 		printf("cr_disable()...\n");
 		cr_disable();
 		i = 0;
-		__do_all_loops(quiet, &i, 20);
+		__do_all_loops(quiet, &i, 10);
 		cr_enable();
 		printf("cr_enable()...\n");
 		i = 0;
-		__do_all_loops(quiet, &i, 20);
+		__do_all_loops(quiet, &i, 10);
 	}
+}
+
+void print_buffer(char *buffer)
+{
+	int i;
+
+	for (i = 0; i < MAX_BUFFER_SIZE; i++) {
+		printf("%c", buffer[i]);
+		if (i % 50 == 0)
+			printf("\n");
+	}
+
+	printf("\n\n");
+}
+
+int restart_cb(void *arg)
+{
+	char *buffer = arg;
+
+	print_buffer(buffer);
+
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
+	char buffer[MAX_BUFFER_SIZE];
+	int i = 0, res;
+	pid_t pid;
+
 	parse_args(argc, argv);
 
 	close_stdioe(close_stdbuffers);
 
 	close_sync_pipe();
 
+	for (i = 0; i < MAX_BUFFER_SIZE; i++)
+		buffer[i]='a';
+
+	buffer[MAX_BUFFER_SIZE-1] = '\0';
+
+	print_buffer(buffer);
+
+/* 	res = cr_exclude_on(buffer, MAX_BUFFER_SIZE, NULL, NULL); */
+/* 	if (res) */
+/* 		fprintf(stderr, "%s:%d - %d - %s\n", */
+/* 			__PRETTY_FUNCTION__, __LINE__, */
+/* 			getpid(), strerror(errno)); */
+
+/* 	res = cr_exclude_off(buffer); */
+/* 	if (res) */
+/* 		fprintf(stderr, "%s:%d - %d - %s\n", */
+/* 			__PRETTY_FUNCTION__, __LINE__, */
+/* 			getpid(), strerror(errno)); */
+
+	res = cr_exclude_on(buffer, MAX_BUFFER_SIZE, restart_cb, buffer);
+	if (res)
+		fprintf(stderr, "%s:%d - %d - %s\n",
+			__PRETTY_FUNCTION__, __LINE__,
+			getpid(), strerror(errno));
+
+	pid = fork();
+
+	res = cr_exclude_on(buffer, MAX_BUFFER_SIZE, NULL, NULL);
+	if (res)
+		fprintf(stderr, "%s:%d - %d - %s\n",
+			__PRETTY_FUNCTION__, __LINE__,
+			getpid(), strerror(errno));
+
 	loop(quiet, numloops);
+	if (pid > 0) {
+		int status;
+		wait(&status);
+		loop(quiet, numloops);
+	}
 
 	return 0;
 }
