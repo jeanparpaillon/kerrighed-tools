88992c971552e3f131b7ae21b5171f24d374264e
Jean Parpaillon <jean.parpaillon@kerlabs.com>
2010-02-03 14:31:17 +0100

    [build] Add automake-1.11 install-sh
    For further modification...

--- clean.sh
+++ clean.sh
@@ -8,8 +8,7 @@
 #
 set -e
 
-AUTOGEN_FILES="aclocal.m4 autom4te.cache configure config.guess config.log config.sub config.status depcomp install-sh compile libtool ltmain.sh missing mki\
-nstalldirs config.h config.h.in m4/*"
+AUTOGEN_FILES="aclocal.m4 autom4te.cache configure config.guess config.log config.sub config.status depcomp compile libtool ltmain.sh missing mkinstalldirs config.h config.h.in m4/*"
 
 echo "Clean autogen generated files"
 for file in $AUTOGEN_FILES; do
