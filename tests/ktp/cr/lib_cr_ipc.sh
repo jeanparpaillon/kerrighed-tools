57f9bc69dbb81a3237114e2e54ca7e580341e7f3
Matthieu Fertré <matthieu.fertre@kerlabs.com>
2010-03-31 14:40:06 +0200

    [KTP] Make function wait_other_instances() reliable in loop mode

--- tests/ktp/cr/lib_cr_ipc.sh
+++ tests/ktp/cr/lib_cr_ipc.sh
@@ -28,8 +28,10 @@
 
 wait_other_instances()
 {
-    local syncfile=$1
-    local counterfunc=$2
+    local counterfunc=$1
+    local syncfile="/tmp/ktp_cr_ipc_sync_${KTP_ROOT_PID}.${KTP_ITERATION}"
+
+    LTP_print_step_info "waiting for every test to be ready"
 
     # waiting for everybody to be ready
     local nr_obj=`eval $counterfunc`
@@ -47,6 +49,8 @@ wait_other_instances()
 	return 1
     fi
     touch $syncfile
+
+    LTP_print_step_info "every tests are ready!"
 
     return 0
 }
